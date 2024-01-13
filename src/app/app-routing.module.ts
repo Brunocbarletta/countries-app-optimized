0c61fe185efbf44bab36cd3421df67bc2a11dbe7
Bruno Cesar Barletta <brunocbarletta@gmail.com>
2024-01-13 16:39:41 -0300

    clase 117 - Cambios iniciales y Rutas basicas establecidas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { AboutPagesComponent } from './shared/pages/about-pages/about-pages.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutPagesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], //forRoot solo se usa para el modulo de rutas principal ubicado en la raiz de nuestro proyecto.
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    AboutPagesComponent,
+    SidebarComponent,
+    ContactPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    AboutPagesComponent,
+    HomePageComponent,
+    SidebarComponent
   ]
 })
 export class SharedModule { }
