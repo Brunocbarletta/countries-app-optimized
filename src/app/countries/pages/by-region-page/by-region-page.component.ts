45f0068467b224f19db0216df0488bccaa6dcad2
Bruno Cesar Barletta <brunocbarletta@gmail.com>
2024-01-15 20:49:26 -0300

    optimizado de seleccion por region con botones

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Ocenia';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,9 +11,12 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Ocenia'];
+  public selectedRegion?: Region;
   private countriesServices = inject(CountriesService);
 
-  searchByRegion(term: string) {
+  searchByRegion(term: Region): void {
+    this.selectedRegion = term;
     this.countriesServices.searchRegion(term)
     .subscribe(countries => {
       this.countries = countries;
